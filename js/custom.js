225c43439c474b56a159588cc7a0186144200a7b
Shohayab Hossain Talha <shohayabhasan@yahoo.com>
2021-11-29 13:15:34 +0600

    CREATED RESPONSIVE MENU FOR MOBILE AND TABLET DEVICE

--- js/custom.js
+++ js/custom.js
@@ -2,3 +2,29 @@
 //     rtl: true
 //   });
       
+
+
+// MAIN-MENU FIXING START FORM HERE 
+const header = document.getElementById("header");
+var navBar = document.getElementById('nav-bar');
+var mainMenu = document.querySelector('.main-menu');
+window.addEventListener('scroll',function(){
+    navBar.classList.remove('color');
+    mainMenu.classList.remove('menuActiveResponsive');
+    var scrolling = document.documentElement.scrollTop;
+    if(scrolling>30)
+    {
+        header.classList.add("headerActiveTop");
+    }
+    else{
+        header.classList.remove("headerActiveTop");
+    }
+})
+// MAIN-MENU FIXING END HERE 
+
+// RESPONSIVE MENU JAVASCRIPT START FORM HERE 
+
+navBar.addEventListener('click',function(){
+    navBar.classList.toggle('color');
+    mainMenu.classList.toggle('menuActiveResponsive');
+})
